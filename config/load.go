a7ebd9d595333cd721a88c3ec395e64ae80115f6
Christian Deacon <gamemann@gflclan.com>
2022-07-21 17:44:49 +0000

    Add more options and function to parse config file.

--- config/load.go
+++ config/load.go
@@ -1,5 +1,11 @@
 package config
 
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+)
+
 type Server struct {
 	UUID int `json:"uuid"`
 
@@ -28,8 +34,12 @@ type Server struct {
 }
 
 type Config struct {
+	// Pterodactyl API.
+	APIEndpoint string `json:"apiurl"`
+	APIToken    string `json:"apitoken"`
+
 	// Paths (e.g. /home/container/server/rust).
-	PathToServerFiles string `json:"pathtoserverfiles"`
+	DefaultPathToServerFiles string `json:"defaultpathtoserverfiles"`
 
 	// Wipe date times.
 	DefaultTimezone string `json:"defaulttimezone"`
@@ -55,4 +65,64 @@ type Config struct {
 	DefaultChatMsgAmount int    `json:"defaultchatmsgamount"`
 
 	Servers []Server `json:"servers"`
+}
+
+// Reads a config file based off of the file name (string) and returns a Config struct.
+func (cfg *Config) LoadConfig(path string) bool {
+	file, err := os.Open(path)
+
+	if err != nil {
+		fmt.Println("[ERR] Cannot open config file.")
+		fmt.Println(err)
+
+		return false
+	}
+
+	defer file.Close()
+
+	stat, _ := file.Stat()
+
+	data := make([]byte, stat.Size())
+
+	_, err = file.Read(data)
+
+	if err != nil {
+		fmt.Println("[ERR] Cannot read config file.")
+		fmt.Println(err)
+
+		return false
+	}
+
+	err = json.Unmarshal([]byte(data), cfg)
+
+	if err != nil {
+		fmt.Println("[ERR] Cannot parse JSON Data.")
+		fmt.Println(err)
+
+		return false
+	}
+
+	return true
+}
+
+// Sets config's default values.
+func (cfg *Config) SetDefaults() {
+	cfg.DefaultPathToServerFiles = "/home/container/server/rust"
+
+	cfg.DefaultTimezone = "America/Chicago"
+	cfg.DefaultWipeTime = ""
+
+	cfg.DefaultDeleteMap = true
+	cfg.DefaultDeleteBP = true
+	cfg.DefaultDeletePD = true
+
+	cfg.DefaultChangeMapSeed = false
+	cfg.DefaultMapSeedsPickType = 1
+
+	cfg.DefaultChangeHostName = true
+	cfg.DefaultHostName = "Vanilla | FULLWIPE {wipetime_one}"
+
+	cfg.DefaultChatMsgEnable = true
+	cfg.DefaultChatMsgAmount = 5
+	cfg.DefaultChatMsg = "Wiping server in {seconds} seconds. Please join back!"
 }
