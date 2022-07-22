f36cce377e79113557d7749804452626535d6af7
Christian Deacon <gamemann@gflclan.com>
2022-07-22 16:35:38 +0000

    Make progress on main file.

--- raw.go
+++ raw.go
@@ -1,5 +1,27 @@
 package main
 
-func main() {
+import (
+	"flag"
+	"github.com/gamemann/Rust-Auto-Wipe/config"
+	"github.com/gamemann/Rust-Auto-Wipe/pterodactyl"
+)
 
+func main() {
+	// Look for 'cfg' flag in command line arguments (default path: /etc/raw/raw.conf).
+	configFile := flag.String("cfg", "/etc/raw/raw.conf", "The path to the Rust Auto Wipe config file.")
+	flag.Parse()
+
+	// Create config struct.
+	cfg := config.Config{}
+
+	// Set config defaults.
+	cfg.SetDefaults()
+
+	// Attempt to read config.
+	cfg.LoadConfig(*configFile)
+
+	// Signal.
+	sigc := make(chan os.Signal, 1)
+	signal.Notify(sigc, syscall.SIGINT, syscall.SIGTERM)
+	<-sigc
 }
