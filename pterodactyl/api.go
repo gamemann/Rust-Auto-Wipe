8223430d2d708971b4b092349c491953c6451716
Christian Deacon <gamemann@gflclan.com>
2022-07-23 18:33:52 +0000

    Start on Pterodactyl API.

--- pterodactyl/api.go
+++ pterodactyl/api.go
@@ -1,1 +1,13 @@
 package pterodactyl
+
+import "github.com/gamemann/Rust-Auto-Wipe/processor"
+
+func SendAPIRequest(wipedata *processor.WipeData, request_type string, request_endpoint string, post_data string) (string, int, error) {
+	d := ""
+	rc := 0
+
+	var e error
+	e = nil
+
+	return d, rc, e
+}
