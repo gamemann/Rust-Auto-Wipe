d17d1a01dc8b713bd25394f02d0add788931cf70
Christian Deacon <gamemann@gflclan.com>
2022-07-25 18:41:58 +0000

    Oops; Return index + 1 and also include UUID in ProcessSeeds.

--- processor/mapseeds.go
+++ processor/mapseeds.go
@@ -1,7 +1,7 @@
 package processor
 
 // Processes seeds and determines the next seed. Should occur before wipe.
-func (wipedata *WipeData) ProcessSeeds() bool {
+func (wipedata *WipeData) ProcessSeeds(UUID string) bool {
 
 	return true
 }
@@ -13,11 +13,11 @@ func (wipedata *WipeData) GetNextSeed(seeds []int, curseed int, picktype int) in
 	if picktype == 1 {
 		for v, s := range seeds {
 			if curseed == s {
-				// If we're on the last seed, return 0 as the array item (starting item). Otherwise, return index.
+				// If we're on the last seed, return 0 as the array item (starting item). Otherwise, return index + 1.
 				if (len(seeds) - 1) == v {
 					seed = 0
 				} else {
-					seed = v
+					seed = v + 1
 				}
 			}
 		}
