85d695d50b13754e0c07183f4dc6d21b4dcff433
Christian Deacon <gamemann@gflclan.com>
2022-07-25 18:30:39 +0000

    Add support for random map seeds.

--- processor/mapseeds.go
+++ processor/mapseeds.go
@@ -7,18 +7,22 @@ func (wipedata *WipeData) ProcessSeeds() bool {
 }
 
 // Gets the next seed in the array.
-func (wipedata *WipeData) GetNextSeed(seeds []int, curseed int) int {
+func (wipedata *WipeData) GetNextSeed(seeds []int, curseed int, picktype int) int {
 	seed := -1
 
-	for v, s := range seeds {
-		if curseed == s {
-			// If we're on the last seed, return 0 as the array item (starting item). Otherwise, return index.
-			if (len(seeds) - 1) == v {
-				s = 0
-			} else {
-				s = v
+	if picktype == 1 {
+		for v, s := range seeds {
+			if curseed == s {
+				// If we're on the last seed, return 0 as the array item (starting item). Otherwise, return index.
+				if (len(seeds) - 1) == v {
+					seed = 0
+				} else {
+					seed = v
+				}
 			}
 		}
+	} else {
+		seed = rand.Intn((len(seeds) - 1) + 1) + 0
 	}
 
 	return seed 
