95813173f9e576a34e0c92fc6db41f4653f81ceb
Christian Deacon <gamemann@gflclan.com>
2022-07-25 18:25:56 +0000

    Add function to pick next map seed.

--- processor/mapseeds.go
+++ processor/mapseeds.go
@@ -5,3 +5,21 @@ func (wipedata *WipeData) ProcessSeeds() bool {
 
 	return true
 }
+
+// Gets the next seed in the array.
+func (wipedata *WipeData) GetNextSeed(seeds []int, curseed int) int {
+	seed := -1
+
+	for v, s := range seeds {
+		if curseed == s {
+			// If we're on the last seed, return 0 as the array item (starting item). Otherwise, return index.
+			if (len(seeds) - 1) == v {
+				s = 0
+			} else {
+				s = v
+			}
+		}
+	}
+
+	return seed 
+}
