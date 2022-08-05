dccb29fe17df52262620c2e8c06aae3392a77e33
Christian Deacon <gamemann@gflclan.com>
2022-08-05 02:58:37 +0000

    Complete map/world seed rotation.

--- internal/processor/mapseeds.go
+++ internal/processor/mapseeds.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"math/rand"
+	"strconv"
 
 	"github.com/gamemann/Rust-Auto-Wipe/internal/pterodactyl"
 )
@@ -19,8 +20,10 @@ func (wipedata *WipeData) ProcessSeeds(UUID string) bool {
 		return false
 	}
 
+	// We want to parse the response with the startup response structure.
 	var EnvVars pterodactyl.StartupResp
 
+	// Convert to JSON.
 	err = json.Unmarshal([]byte(d), &EnvVars)
 
 	if err != nil {
@@ -28,14 +31,53 @@ func (wipedata *WipeData) ProcessSeeds(UUID string) bool {
 
 		return false
 	}
+
+	// Default seed = empty.
+	cur_seed_str := ""
+
+	// Loop through all startup variables and find the seed env value.
+	for _, seed := range EnvVars.Data {
+		if seed.Attributes.Env_Variable == "WORLD_SEED" {
+			cur_seed_str = seed.Attributes.Srv_Value
+		}
+	}
+
+	// Let's use ParseInt() for flexability.
+	curseed64, err := strconv.ParseInt(cur_seed_str, 10, 16)
+
+	if err != nil {
+		fmt.Println(err)
+
+		return false
+	}
+
+	// Convert to integer type (To Do: Find a less sloppy way of doing this).
+	cur_seed := int(curseed64)
+
+	// Now get the next seed using the GetNextSeed() method.
+	next_seed := wipedata.GetNextSeed(cur_seed)
+
+	// Now convert to proper POST data.
+	var post_data map[string]string
+	post_data["key"] = "WORLD_SEED"
+	post_data["value"] = strconv.Itoa(next_seed)
+
+	// Send API request.
+	d, _, err = pterodactyl.SendAPIRequest(wipedata.APIURL, wipedata.APIToken, "PUT", "client/servers/"+UUID+"/variable", post_data)
+
 	return true
 }
 
 // Gets the next seed in the array.
-func (wipedata *WipeData) GetNextSeed(seeds []int, curseed int, picktype int) int {
+func (wipedata *WipeData) GetNextSeed(curseed int) int {
+	// Make new variables for better looking code.
 	seed := -1
+	seeds := wipedata.MapSeeds
+	pick_type := wipedata.MapSeedPickType
 
-	if picktype == 1 {
+	// Check pick type.
+	if pick_type == 1 {
+		// Loop through all seeds and get the next seed.
 		for v, s := range seeds {
 			if curseed == s {
 				// If we're on the last seed, return 0 as the array item (starting item). Otherwise, return index + 1.
