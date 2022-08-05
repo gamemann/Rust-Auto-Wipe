462d821df6ddf275558b154ffad7ef00b6ef1208
Christian Deacon <gamemann@gflclan.com>
2022-08-05 08:08:17 +0000

    Implement send message (command) function.

--- internal/wipe/warnings.go
+++ internal/wipe/warnings.go
@@ -1,1 +1,16 @@
 package wipe
+
+import "github.com/gamemann/Rust-Auto-Wipe/pkg/pterodactyl"
+
+func SendMessage(data *Data, UUID string, message string) error {
+	var err error
+
+	// Now convert to proper POST data.
+	var post_data map[string]interface{}
+	post_data["command"] = "say " + message
+
+	// Send API request to update host name variable.
+	_, _, err = pterodactyl.SendAPIRequest(data.APIURL, data.APIToken, "POST", "client/servers/"+UUID+"/POST", post_data)
+
+	return err
+}
