cf5dc5b0431a5f98963f0f48207029304dcbecca
Christian Deacon <gamemann@gflclan.com>
2022-08-05 07:32:04 +0000

    Implement function to check if server is running.

--- internal/wipe/server.go
+++ internal/wipe/server.go
@@ -1,8 +1,25 @@
 package wipe
 
 import (
+	"encoding/json"
+
 	"github.com/gamemann/Rust-Auto-Wipe/pkg/pterodactyl"
 )
+
+type ServerResources struct {
+	Object     string `json:"object"`
+	Attributes struct {
+		CurrentState string `json:"current_state"`
+		IsSuspended  bool   `json:"is_suspended"`
+		Resources    struct {
+			MemoryBytes    int `json:"memory_bytes"`
+			CPUAbsolute    int `json:"cpu_absolute"`
+			DiskBytes      int `json:"disk_bytes"`
+			NetworkRxBytes int `json:"network_rx_bytes"`
+			NetworkTxBytes int `json:"network_tx_bytes"`
+		} `json:"resources"`
+	} `json:"attributes"`
+}
 
 func StartServer(data *Data, UUID string) error {
 
@@ -23,6 +40,31 @@ func KillServer(data *Data, UUID string) error {
 	err := SendPowerCommand(data, UUID, "kill")
 
 	return err
+}
+
+func IsServerRunning(data *Data, UUID string) (bool, error) {
+	var running bool = false
+	var err error
+
+	d, _, err := pterodactyl.SendAPIRequest(data.APIURL, data.APIToken, "GET", "client/servers/"+UUID+"/resources", nil)
+
+	if err != nil {
+		return running, err
+	}
+
+	var resources ServerResources
+
+	err = json.Unmarshal([]byte(d), &resources)
+
+	if err != nil {
+		return running, err
+	}
+
+	if resources.Attributes.CurrentState == "running" {
+		running = true
+	}
+
+	return running, err
 }
 
 func SendPowerCommand(data *Data, UUID string, cmd string) error {
