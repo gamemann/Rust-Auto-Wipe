463ba075c4347388a88781d7d01acfce643774f8
Christian Deacon <gamemann@gflclan.com>
2022-08-06 21:46:36 +0000

    Add local time to debug messages.

--- pkg/debug/print.go
+++ pkg/debug/print.go
@@ -3,15 +3,18 @@ package debug
 import (
 	"fmt"
 	"strconv"
+	"time"
 )
 
 func SendDebugMsg(UUID string, debug_level int, required_level int, message string) {
 	if debug_level >= required_level {
+		now := time.Now().Local()
+
 		pre := "ERR"
 
 		if required_level > 0 {
 			pre = strconv.Itoa(required_level)
 		}
-		fmt.Println("[" + pre + "] " + UUID + " :: " + message)
+		fmt.Println("[" + pre + "][" + now.Format("1-2 03:04:05 PM") + "] " + UUID + " :: " + message)
 	}
 }
